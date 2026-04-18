862f852d75b322b4b5ab7c61e0ac9a17c5b1876e
AKZero Agency <106025510+akzeronet@users.noreply.github.com>
2026-04-18 15:25:08 -0400

    Update litellm_license.py
    modif

--- litellm/proxy/auth/litellm_license.py
+++ litellm/proxy/auth/litellm_license.py
@@ -22,7 +22,7 @@ class LicenseCheck:
     - Returns if license is valid
     """
 
-    base_url = "https://demo.demo.cloudron.io"
+    base_url = "https://lamp.demo.cloudron.io"
 
     def __init__(self) -> None:
         self.license_str = os.getenv("LITELLM_LICENSE", None)
